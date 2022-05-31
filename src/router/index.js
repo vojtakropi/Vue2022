084d9ac88bc15bd086ca7c5c8f493ae00e1ea449
Vojtěch Kropáček <kropacekv@btlnet.com>
2022-05-31 12:18:29 +0200

    Create index.js

---
+++ src/router/index.js
@@ -0,0 +1,17 @@
+import Vue from "vue"
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+import views from '../views/home'
+
+const routes = [{
+    component: views,
+    name: 'home',
+    path: '/home'
+},
+];
+
+export default new VueRouter[{
+    routes
+}];
